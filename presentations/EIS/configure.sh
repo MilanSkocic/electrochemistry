e47b2efa13a1f741ee5b657b4115a4756078735b
Milan Skocic <milan.skocic@gmail.com>
2025-11-05 15:25:23 +0100

    Update presentations.

--- presentations/EIS/configure.sh
+++ presentations/EIS/configure.sh
@@ -1,13 +1,34 @@
 #!/usr/bin/env bash
 
-url=../../../3-Resource/logo/png
+BUILD_DIR=build
+DEP_DIR=build/dependencies
+GIT="bibfiles drawings"
+
+mkdir -p $BUILD_DIR
+mkdir -p $DEP_DIR
+
+cd $DEP_DIR
+
+for i in $GIT;do
+    url="https://github.com/MilanSkocic/$i.git"
+    if [[ ! -d $i/ ]]; then 
+        git clone $url; 
+        if [[ $?>0 ]]; then 
+            echo "The repo $i could not be retrieved."; 
+        fi
+    fi
+done
+
+cd ../../
+
+url=$DEP_DIR/drawings/logo/png
 target=full_bw.png
 cp -fv $url/$target ./src/figures/
 
-url=../../../3-Resource/figures/png
+url=$DEP_DIR/drawings/figures/png
 target=EIS-*.png
 cp -fv $url/$target ./src/figures/
 
-url=../../../3-Resource/references
+url=$DEP_DIR/bibfiles
 target=references.bib
 cp -fv $url/$target ./src/references.bib
